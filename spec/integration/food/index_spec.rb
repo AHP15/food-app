541ef9212089cc652b4593b0dd286633f6ad223a
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-03-15 12:58:30 +0100

    Add public recipe page, style for it and tests

--- spec/integration/food/index_spec.rb
+++ spec/integration/food/index_spec.rb
@@ -1,9 +1,10 @@
+require 'rails_helper'
+
 RSpec.describe 'Foods', type: :system do
   before :all do
     @food = Food.create(
       name: 'apple', measurement_unit: 'kg', price: 5, quantity: 10, user_id: 1
     )
-    @food.save
   end
 
   it 'shows the right content for index' do
